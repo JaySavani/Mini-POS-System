6dec3a2054a8ddc84e5d5c19dcc0dd698a4f33bd
JaySavani <jaduhacker2024@gmail.com>
2026-03-06 00:14:48 +0530

    fix: build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import { Footer } from "@/components/footer/footer";
+import { Footer } from "@/components/Footer/footer";
 import { Navbar } from "@/components/navbar/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
